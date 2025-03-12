88cd34a74e40078a543aec910d77de0d2ecf6331
bhindor <bhindor@dgsw.hs.kr>
2025-03-12 17:26:43 +0900

    printf practice
    printf 기초

---
+++ week01/printf/printf_test.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+//printf_test 1 //printf 기초
+/*int main() {
+    printf("My name is Hong Gil Dong.\n");
+    printf("I am 13 years old.");
+
+    return 0;
+}*/
+
+//printf_test 2
+/*int main() {
+    printf("(@) (@)\n");
+    printf("(=^.^=)\n");
+    printf("(-m-m-)\n");
+
+    return 0;
+}*/
+
+//printf_test 3 //printf %d-정수, %f-실수, %c-문자, %s-문자열
+/*int main() {
+    printf("%s \n", "I can program well.");
+    printf("%s \n", "Dreams come true.");
+
+    return 0;
+}*/
+
+//printf_test 4 //문자열, 정수, 실수 출력
+// int main() { 
+//     printf("%s\n", "My Height");
+//     printf("%d\n", 170);
+//     printf("%s\n", "My weight");
+//     printf("%f\n", 68.6);
+
+//     return 0;
+// }
+
+//printf_test 5 //정수 계산
+int main() {
+    printf("5 Dan\n");
+    printf("%d * %d = %d\n", 5, 2, 5 * 2);
+    return 0;
+}
